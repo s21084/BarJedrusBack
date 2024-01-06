6e700c66c65efc2814b9aa53e3d7fd5f5b0b4274
s21084 <s21084@pjwstk.edu.pl>
2024-01-06 19:38:04 +0100

    small changes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -12,7 +12,7 @@ const prisma = new PrismaClient();
 
 // Generate a random 8 digit number as the email token
 function generateEmailToken(): string {
-  return Math.floor(10000000 + Math.random() * 90000000).toString();
+  return Math.floor(10000 + Math.random() * 90000).toString();
 }
 
 function generateAuthToken(tokenId: number): string {
