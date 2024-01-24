33231192cdfade7a72b34dbab90e5b20f4aecb0b
s21084 <s21084@pjwstk.edu.pl>
2024-01-24 18:13:48 +0100

    Schedule

--- src/routes/scheduleRoute.ts
+++ src/routes/scheduleRoute.ts
@@ -27,27 +27,55 @@ router.post('/', async (req, res) => {
 });
 
 
-//List Schedule
+//List Schedule DONE
 router.get('/', async (req, res) => {
-    res.status(501).json({error: "Not implemented get list"})
+    const allSched = await prisma.schedule.findMany({include:{user: true} });
+    res.json(allSched);
 });
 
-//Get one Schedule
-router.get('/:id', async (req, res) => {
+//Get one Schedule DONE
+router.get('/id/:id', async (req, res) => {
     const { id } = req.params;
-    res.status(501).json({error: `Not implemented get one : ${id}`})
+    const sched = await prisma.schedule.findUnique({where: {id: Number(id)}, include:{user: true}} );
+    res.json(sched);
 });
 
-//Update Schedule
+
+//DONE
+router.get('/user/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const schedule = await prisma.schedule.findMany({where: {userId: Number(userId)}, include:{user: true}} );
+    res.json(schedule);
+});
+
+//Update Schedule DONE
 router.put('/:id', async (req, res) => {
     const { id } = req.params;
-    res.status(501).json({error: `Not implemented update : ${id}`})
+    const { startHour, endHour, weekDayNumber, userId, informationBarId} = req.body;
+
+    try{
+        const result = await prisma.schedule.update({
+            where: { id: Number(id)},
+            data: {
+                startHour,
+                endHour,
+                weekDayNumber,
+                userId,
+                informationBarId
+            },
+        });
+        res.json(result);
+    } catch (e) {
+        res.status(400).json({error: "Unable to update"})
+    }
+
 });
 
-//Delete Schedule
+//Delete Schedule DONE
 router.delete('/:id', async (req, res) => {
     const { id } = req.params;
-    res.status(501).json({error: `Not implemented delete : ${id}`})
+    await prisma.schedule.delete({where: {id: Number(id)}})
+    res.sendStatus(200);
 });
 
 
